Fixed attention mask

models/gpt.py
@@ -34,10 +34,10 @@ class GPT(nn.Module):
         p = self.position_embedding(torch.arange(S).to(x.device)) # (S, d_embed)
         p = p.unsqueeze(0).repeat(B, 1, 1) # (B, S, d_embed)
         x = e + p # (B, S, d_embed)
-
+        
         for block in self.transformer_blocks:
             x = block(x)
-
+        
         x = self.ln_f(x)
 
         logits = self.lm_head(x)

models/model_components/attention.py
@@ -81,8 +81,7 @@ class Attention(nn.Module):
         k = k.view(B, S, self.n_heads, self.d_attn).transpose(1, 2) # (B, n_heads, S, d_attn)
         v = v.view(B, S, self.n_heads, self.d_attn).transpose(1, 2) # (B, n_heads, S, d_attn)
     
-        # Create causal mask
-        causal_mask = torch.triu(torch.ones(S, S), diagonal=1).bool()
+        causal_mask = torch.triu(torch.ones(S, S), diagonal=1).bool().logical_not()
         causal_mask = causal_mask.to(q.device)
 
         attn_scores = self.attn_fn(q, k, self.scale, self.gamma, causal_mask) # (B, n_heads, S, S)