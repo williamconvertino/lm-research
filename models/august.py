c13674454d062cc8170e9502a1a1fbcfc65af078
William Convertino <williamconvertino@gmail.com>
2025-02-14 14:16:18 -0500

    Implemented august model and added configs

--- models/august.py
+++ models/august.py
@@ -2,66 +2,122 @@ import torch
 import math
 import torch.nn as nn
 import torch.nn.functional as F
-from .model_util.zero_mean_embedding import TopicalEmbedding
-from .model_util.feed_forward import FeedForward
-from .model_util.attention_functions import Attention
+from torchtune.modules import RotaryPositionalEmbeddings
+from models.model_util.base_model import BaseModel
+from models.model_util.zero_mean_embedding import ZeroMeanEmbedding
 
-class August(nn.Module):
+class Attention(nn.Module):
+    def __init__(self, d_embed, n_heads):
+        super().__init__()
+        
+        self.d_embed = d_embed
+        self.n_heads = n_heads
+        
+        self.W_q = nn.Linear(self.d_embed, self.d_embed, bias=False)
+        self.W_k = nn.Linear(self.d_embed, self.d_embed, bias=False)
+        self.W_v = nn.Linear(self.d_embed, self.d_embed, bias=False)
+        self.out_proj = nn.Linear(self.d_embed, self.d_embed, bias=False)
+
+        self.rotary_embedding = RotaryPositionalEmbeddings(self.d_embed // self.n_heads)
+
+        self.attn_dropout = nn.Dropout(0.1)
+        self.out_dropout = nn.Dropout(0.1)
+
+    def forward(self, q, k, v):
+        B, S, _ = q.shape
+        
+        q = self.W_q(q) # (B, S, d_attn * n_heads)
+        k = self.W_k(k) # (B, S, d_attn * n_heads)
+        v = self.W_v(v) # (B, S, d_attn * n_heads)
+
+        q = q.view(B, S, self.n_heads, self.d_embed // self.n_heads) # (B, S, n_heads, d_attn)
+        k = k.view(B, S, self.n_heads, self.d_embed // self.n_heads) # (B, S, n_heads, d_attn)
+        v = v.view(B, S, self.n_heads, self.d_embed // self.n_heads) # (B, S, n_heads, d_attn)
+        
+        q = self.rotary_embedding(q)
+        k = self.rotary_embedding(k)
+
+        q = q.transpose(1, 2) # (B, n_heads, S, d_attn)
+        k = k.transpose(1, 2) # (B, n_heads, S, d_attn)
+        v = v.transpose(1, 2) # (B, n_heads, S, d_attn)
+
+        causal_mask = torch.triu(torch.ones(S, S), diagonal=1).bool()
+        causal_mask = causal_mask.to(q.device)
+
+        attn_scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(self.d_embed // self.n_heads) # (B, n_heads, S, S)
+        attn_scores = attn_scores.masked_fill(causal_mask, float("-inf"))
+        attn_scores = F.softmax(attn_scores, dim=-1)
+        attn_scores = self.attn_dropout(attn_scores)
+
+        attn = attn_scores @ v # (B, n_heads, S, d_attn)
+        attn = attn.transpose(1, 2).contiguous().view(B, S, self.d_embed) # (B, S, d_embed)
+        out = self.out_proj(attn) # (B, S, d_embed)
+        out = self.out_dropout(out)
+
+        return out
+
+class FeedForward(nn.Module):
+    def __init__(self, d_in, d_out, d_embed):
+        super().__init__()
+
+        self.ln = nn.LayerNorm(d_in)
+
+        self.fc1 = nn.Linear(d_in, 4 * self.d_embed)
+        self.fc2 = nn.Linear(4 * self.d_embed, d_out)
+
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        x = self.ln(x)
+        x = self.fc1(x)
+        x = F.gelu(x)
+        x = self.fc2(x)
+        x = self.dropout(x)
+        return x
+
+class TransformerBlock(nn.Module):
+    def __init__(self, d_embed, n_heads):
+        super().__init__()
+        self.d_aug = d_embed // 3
+        self.attention = Attention(d_embed, n_heads)
+        self.feed_forward = FeedForward(2 * self.d_aug, self.d_aug, d_embed)
+        
+    def forward(self, f, g, e):
+        f = self.attention(q=e, k=e, v=g)
+        g = self.feed_forward(torch.cat([f, e], dim=-1))
+        return f, g, e
+
+class August(BaseModel):
     def __init__(self, config):
         super().__init__()
         self.config = config
+
+        self.embedding = ZeroMeanEmbedding(config.vocab_size, config.d_embed // 3)
         
-        self.aug_d_embed = config.d_embed // 3
+        self.transformer_blocks = nn.ModuleList([TransformerBlock(config.d_embed, config.n_heads) for _ in range(config.n_layers)])
 
-        self.embedding = nn.Embedding(config.vocab_size, self.aug_d_embed)
+        self.ln_f = nn.LayerNorm(config.d_embed // 3)
+
+        self.lm_head = nn.Linear(config.d_embed // 3, config.vocab_size, bias=False)
+        self.embedding.tie_weights(self.lm_head)
         
-        self.attention_blocks = nn.ModuleList([Attention(config) for _ in range(config.n_layers)])
-        self.feed_forward_blocks = nn.ModuleList([FeedForward(config.d_embed) for _ in range(config.n_layers)])
-
-        self.ln_f = nn.LayerNorm(config.d_embed)
-
-        self.lm_head = nn.Linear(config.d_embed, config.vocab_size, bias=False)
-        self.lm_head.weight = self.embedding.weight
-
-        self.apply(self._init_weights)
+        self.init_weights()
         
-    def _init_weights(self, module):
-        if isinstance(module, nn.LayerNorm):
-            nn.init.ones_(module.weight)
-            if module.bias is not None:
-                nn.init.zeros_(module.bias)
-        elif isinstance(module, nn.Linear):
-            nn.init.xavier_normal_(module.weight)
-            if module.bias is not None:
-                nn.init.zeros_(module.bias)
-        elif isinstance(module, nn.Embedding):
-            nn.init.normal_(module.weight, mean=0.0, std=0.02)
-
     def forward(self, x, targets=None):
         B, S = x.shape
 
-        e = self.embedding(x) - self.embedding.weight.mean(dim=0, keepdim=True) # (B, S, d_embed)
+        e = self.embedding(x) # (B, S, d_embed // 3)
+        g = e
+        f = torch.zeros_like(e, device=e.device)
         
-        x = torch.cat([torch.zeros_like(e), e, e], dim=-1) # (B, S, 3 * aug_d_embed)
-        
-            
-        for i in range(self.config.n_layers):
-            v = torch.cat([x[:, :, self.aug_d_embed:2 * self.aug_d_embed], torch.zeros_like(x[:, :, :2 * self.aug_d_embed])], dim=-1)
-            x = x + self.attention_blocks[i](q=x, k=x, v=v)
-            
-            if i < self.config.n_layers - 1:
-                ff_in = torch.cat([x[:, :, :self.aug_d_embed], torch.zeros_like(x[:, :, 2 * self.aug_d_embed:]), x[:, :, 2 * self.aug_d_embed:]], dim=-1)
-            else:
-                ff_in = x
+        for block in enumerate(self.transformer_blocks):
+            f, g, e = block(f, g, e)
 
-            x = x + self.feed_forward_blocks[i](ff_in)
-
-        x = self.ln_f(x)
-        x = x[:, :, :self.aug_d_embed]
+        x = self.ln_f(f)
 
         logits = self.lm_head(x)
         if targets is None:
             return logits, None
     
-        loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.contiguous().view(-1), ignore_index=self.config.tokenizer.pad_id)
+        loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.contiguous().view(-1))
         return logits, loss
