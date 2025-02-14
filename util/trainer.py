f19f95ca847535e839cfe1e740174704edf8da7f
William Convertino <williamconvertino@gmail.com>
2025-02-13 21:30:56 -0500

    Fixed broken reference

--- util/trainer.py
+++ util/trainer.py
@@ -3,7 +3,7 @@ import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.optim.lr_scheduler import get_cosine_schedule_with_warmup
+from transformers import get_cosine_schedule_with_warmup
 import math
 
 class Trainer:
