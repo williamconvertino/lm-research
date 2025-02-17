c98886679149257d1f7baebbf7e67515831e463e
William Convertino <williamconvertino@gmail.com>
2025-02-17 01:04:38 -0500

    Updated GPT and added october model

--- models/october.py
+++ models/october.py
@@ -12,10 +12,11 @@ class Attention(nn.Module):
         
         self.d_embed = d_embed
         self.n_heads = n_heads
+        self.d_aug = d_embed // 3
         
         self.W_q = nn.Linear(self.d_embed, self.d_embed, bias=False)
         self.W_k = nn.Linear(self.d_embed, self.d_embed, bias=False)
-        self.W_v = nn.Linear(self.d_embed, self.d_embed, bias=False)
+        self.W_v = nn.Linear(self.d_aug, self.d_aug, bias=False)
         self.out_proj = nn.Linear(self.d_embed, self.d_embed, bias=False)
 
         self.rotary_embedding = RotaryPositionalEmbeddings(self.d_embed // self.n_heads)
@@ -28,7 +29,9 @@ class Attention(nn.Module):
         
         q = self.W_q(q) # (B, S, d_attn * n_heads)
         k = self.W_k(k) # (B, S, d_attn * n_heads)
-        v = self.W_v(v) # (B, S, d_attn * n_heads)
+        v = self.W_v(v[:, :, self.d_aug: 2 * self.d_aug]) # (B, S, d_attn * n_heads)
+
+        v = torch.cat([v, torch.zeros_like(v, device=v.device), torch.zeros_like(v, device=v.device)], dim=-1)
 
         q = q.view(B, S, self.n_heads, self.d_embed // self.n_heads) # (B, S, n_heads, d_attn)
         k = k.view(B, S, self.n_heads, self.d_embed // self.n_heads) # (B, S, n_heads, d_attn)
@@ -59,14 +62,16 @@ class Attention(nn.Module):
 class FeedForward(nn.Module):
     def __init__(self, d_embed):
         super().__init__()
-        self.d_embed = d_embed
 
-        self.fc1 = nn.Linear(self.d_embed, 4 * self.d_embed)
-        self.fc2 = nn.Linear(4 * self.d_embed, self.d_embed)
+        self.ln = nn.LayerNorm(d_embed)
+
+        self.fc1 = nn.Linear(d_embed, 4 * d_embed)
+        self.fc2 = nn.Linear(4 * d_embed, d_embed)
 
         self.dropout = nn.Dropout(0.1)
 
     def forward(self, x):
+        x = self.ln(x)
         x = self.fc1(x)
         x = F.gelu(x)
         x = self.fc2(x)
@@ -77,6 +82,7 @@ class TransformerBlock(nn.Module):
     def __init__(self, d_embed, n_heads):
         super().__init__()
 
+        self.d_aug = d_embed // 3
         self.attention = Attention(d_embed, n_heads)
         self.feed_forward = FeedForward(d_embed)
 
@@ -84,25 +90,30 @@ class TransformerBlock(nn.Module):
         self.ln_2 = nn.LayerNorm(d_embed)
         
         
-    def forward(self, x):
+    def forward(self, x, final=False):
         x = self.ln_1(x)
         x = x + self.attention(x, x, x)
         x = self.ln_2(x)
-        x = x + self.feed_forward(x)
+        if final:
+            x = torch.cat([x[:, :, :self.d_aug], torch.zeros_like(x[:, :, self.d_aug:], device=x.device)], dim=-1)
+        else:
+            ff_in = torch.cat([x[:, :, :self.d_aug], torch.zeros_like(x[:, :, :self.d_aug], device=x.device), x[:, :, 2 * self.d_aug:]], dim=-1)
+            x = x + self.feed_forward(ff_in)
         return x
+        
 
-class GPT(BaseModel):
+class October(BaseModel):
     def __init__(self, config):
         super().__init__()
         self.config = config
 
-        self.embedding = ZeroMeanEmbedding(config.vocab_size, config.d_embed)
+        self.embedding = ZeroMeanEmbedding(config.vocab_size, config.d_embed // 3)
         
         self.transformer_blocks = nn.ModuleList([TransformerBlock(config.d_embed, config.n_heads) for _ in range(config.n_layers)])
 
-        self.ln_f = nn.LayerNorm(config.d_embed)
+        self.ln_f = nn.LayerNorm(config.d_embed // 3)
 
-        self.lm_head = nn.Linear(config.d_embed, config.vocab_size, bias=False)
+        self.lm_head = nn.Linear(config.d_embed // 3, config.vocab_size, bias=False)
         self.embedding.tie_weights(self.lm_head)
         
         self.init_weights()
@@ -110,11 +121,15 @@ class GPT(BaseModel):
     def forward(self, x, targets=None):
         B, S = x.shape
 
-        x = self.embedding(x) # (B, S, d_embed)
-
-        for block in self.transformer_blocks:
-            x = block(x)
+        f = torch.zeros_like(e, device=e.device)
+        e = g = self.embedding(x) # (B, S, d_embed // 3)
         
+        x = torch.cat([f, g, e], dim=-1)
+
+        for i, block in enumerate(self.transformer_blocks):
+            x = block(x, final=(i == len(self.transformer_blocks) - 1))
+
+        x = x[:, :, :self.d_embed // 3]
         x = self.ln_f(x)
 
         logits = self.lm_head(x)
