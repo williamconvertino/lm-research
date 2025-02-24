6e548aa73af06f4043ddf85afaa47219fee3f403
William Convertino <williamconvertino@gmail.com>
2025-02-23 19:40:44 -0500

    Fixed import error

--- main.py
+++ main.py
@@ -5,32 +5,7 @@ from util.trainer import Trainer
 from util.evaluator import Evaluator
 from dataset.tokenizer import Tokenizer
 from dataset.tinystories_dataset import TinyStoriesDataset
-from util.loading import load_most_recent_checkpoint, load_config
-import os
-import importlib
-
-MODELS_DIR = os.path.join(os.path.dirname(__file__), "models")
-
-def load_model(config):
-    
-    model_name = config.model
-    model_dir = os.path.join(MODELS_DIR)
-    model_cls = None
-    
-    try:
-        model_file = importlib.import_module(model_dir)
-    except ModuleNotFoundError:
-        raise ValueError(f"Model file not found: {model_dir}")
-    
-    for attr in dir(model_file): # Find class in module
-        if attr.lower() == model_name.lower().replace("_", ""):
-            model_cls = getattr(model_file, attr)
-        break
-    
-    if model_cls is None:
-        raise ValueError(f"Model class not found: {model_name}")    
-        
-    return model_cls(config)
+from util.loading import load_model, load_most_recent_checkpoint, load_config
 
 def main():
     parser = ArgumentParser()
--- util/loading.py
+++ util/loading.py
@@ -1,9 +1,12 @@
 import os
 import torch
+import importlib
 from types import SimpleNamespace
 import json
+import sys
 
 CHECKPOINT_DIR = os.path.join(os.path.dirname(__file__), "../checkpoints")
+MODELS_DIR = os.path.join(os.path.dirname(__file__), "../models")
 CONFIG_DIR = os.path.join(os.path.dirname(__file__), "../configs")
 
 def load_most_recent_checkpoint(model):
@@ -11,6 +14,28 @@ def load_most_recent_checkpoint(model):
     if not os.path.exists(checkpoint_path):
         return None
     return torch.load(checkpoint_path)
+
+def load_model(config):
+    
+    sys.path.append(MODELS_DIR)
+    model_name = config.model
+    module_name = f".models.{model_name}"
+    model_cls = None
+    
+    try:
+        model_file = importlib.import_module(module_name)
+    except ModuleNotFoundError:
+        raise ValueError(f"Module not found: {module_name}")
+    
+    for attr in dir(model_file): # Find class in module
+        if attr.lower() == model_name.lower().replace("_", ""):
+            model_cls = getattr(model_file, attr)
+        break
+    
+    if model_cls is None:
+        raise ValueError(f"Model class not found: {model_name}")    
+        
+    return model_cls(config)
 
 def load_config(config_name):
     
