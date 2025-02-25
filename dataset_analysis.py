d86c637195f156d3d573bac68f1cdfc47b730f52
William Convertino <williamconvertino@gmail.com>
2025-02-24 20:43:31 -0500

    Added dataset analysis

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -4,9 +4,15 @@ HUGGINGFACE_PATH = "bookcorpus"
 
 dataset = load_dataset(HUGGINGFACE_PATH, cache_dir=f"../data/datasets/raw", streaming=True)["train"]
 
+count = 0
+
 for i, example in enumerate(dataset):
     print("=" * 10)
     if "=" in example["text"]:
         print(example["text"])
+        count = 3
+    if count > 0:
+        print(example["text"])
+    count -= 1
     if i > 1000:
         break
