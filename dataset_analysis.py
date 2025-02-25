03bc1494cd8f4279e20847d68ee9ff9c6fd77702
William Convertino <williamconvertino@gmail.com>
2025-02-24 20:48:51 -0500

    Added dataset analysis

--- dataset_analysis.py
+++ dataset_analysis.py
@@ -6,9 +6,12 @@ HUGGINGFACE_PATH = "bookcorpus"
 dataset = load_dataset(HUGGINGFACE_PATH, cache_dir=f"../data/datasets/raw", streaming=True)["train"]
 
 
+bad_chars = ['=', '>', '<', '[', ']', '{', '}', '|', '\\', '/', '*', '+', '-', '&', '%', '$', '#', '@', '^', '~', '`']
+             
 for i, example in enumerate(dataset):
-    text = example['text']
-    if re.search(r'[^a-zA-Z0-9\s\p{P}]', text):
-        print(text)
-    if i > 1000:
+    
+    if any(char in example["text"] for char in bad_chars):
+        print(f"{example['text']}")
+    
+    if i > 10000:
         break
