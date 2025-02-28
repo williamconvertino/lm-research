de38225f4c0fdf890e75cbb49b09b071af16144d
William Convertino <williamconvertino@gmail.com>
2025-02-28 00:53:38 -0500

    Added updated b models

--- models/b0nl.py
+++ models/b0nl.py
@@ -98,7 +98,7 @@ class TransformerBlock(nn.Module):
         
         return ex, f_plus
 
-class B0e(nn.Module):
+class B0nl(nn.Module):
     def __init__(self, config):
         super().__init__()
         
