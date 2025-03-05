56aeec824b34593b6d5717b7f5b40618777912d9
William Convertino <williamconvertino@gmail.com>
2025-03-04 22:46:09 -0500

    Added sor model

---
+++ models/sor.py
@@ -0,0 +1,148 @@
+import torch
+import math
+import torch.nn as nn
+import torch.nn.functional as F
+from torchtune.modules import RotaryPositionalEmbeddings
+
+class Attention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+        self.config = config
+        
+        self.W_q = nn.Parameter(torch.zeros(config.n_heads, config.d_half, config.d_embed))
+        self.W_k = nn.Parameter(torch.zeros(config.n_heads, config.d_half, config.d_embed))
+        self.W_v = nn.Parameter(torch.zeros(config.n_heads, config.d_half, config.d_embed))
+        self.W_o = nn.Linear(config.n_heads * config.d_embed, config.d_embed, bias=False)
+        
+        self.attn_scale = 1 / math.sqrt(config.d_embed)
+        
+        self.rotary_embeddings = RotaryPositionalEmbeddings(config.d_embed)
+        
+        self.drop_attn = nn.Dropout(0.1)
+        self.drop_resid = nn.Dropout(0.1)
+        
+    def forward(self, q, k=None, v=None):
+        
+        B, S, E = q.shape
+        
+        if k is None:
+            k = q
+        if v is None:
+            v = q
+        
+        q = q.unsqueeze(2).expand(B, S, self.config.n_heads, self.config.d_half) # (B, S, n_heads, d_embed)
+        k = k.unsqueeze(2).expand(B, S, self.config.n_heads, self.config.d_half)
+        v = v.unsqueeze(2).expand(B, S, self.config.n_heads, self.config.d_half)
+            
+        q = torch.einsum('b s h e, h e d -> b s h d', q, self.W_q) # (B, S, n_heads, d_embed)
+        k = torch.einsum('b s h e, h e d -> b s h d', k, self.W_k)
+        v = torch.einsum('b s h e, h e d -> b s h d', v, self.W_v)
+        
+        q = self.rotary_embeddings(q) # (B, S, n_heads, d_embed)
+        k = self.rotary_embeddings(k)
+        
+        q = q.transpose(1, 2) # (B, n_heads, S, d_embed)
+        k = k.transpose(1, 2)
+        v = v.transpose(1, 2)
+        
+        causal_mask = torch.triu(torch.ones(S, S), diagonal=1).bool().to(q.device)
+        
+        attn_scores = torch.matmul(q, k.transpose(-2, -1)) * self.attn_scale
+        attn_scores = attn_scores.masked_fill(causal_mask, float('-inf'))
+        
+        attn_probs = F.softmax(attn_scores, dim=-1)
+        attn_probs = self.drop_attn(attn_probs)
+        
+        attn_output = torch.matmul(attn_probs, v)
+        attn_output = attn_output.transpose(1, 2).contiguous().view(B, S, self.config.d_embed * self.config.n_heads)
+        attn_output = self.W_o(attn_output)
+        attn_output = self.drop_resid(attn_output)
+        
+        return attn_output
+        
+class FeedForward(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.fc_1 = nn.Linear(config.d_half, 4 * config.d_embed)
+        self.fc_2 = nn.Linear(4 * config.d_embed, config.d_half)
+        
+        self.activation = nn.GELU()    
+        self.drop = nn.Dropout(0.1)
+
+    def forward(self, x):
+        x = self.fc_1(x)
+        x = self.activation(x)
+        x = self.drop(x)
+        x = self.fc_2(x)
+        return x
+
+class TransformerBlock(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+        self.attention = Attention(config)
+        self.feed_forward = FeedForward(config)
+        self.ln_f = nn.LayerNorm(config.d_half)
+        self.ln_ex = nn.LayerNorm(config.d_half)
+        self.ln_ff = nn.LayerNorm(config.d_half)
+        
+    def forward(self, ex, f):
+        qk = self.ln_f(f)
+        v = self.ln_ex(ex)
+        f = f + self.attention(q=qk, k=qk, v=v)
+        ex = self.feed_forward(self.ln_ff(f))
+        return ex, f
+
+class SOR(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+        self.config = config
+        self.config.d_half = config.d_embed // 2
+        self.embedding = nn.Embedding(config.vocab_size, config.d_half)
+
+        self.transformer_blocks = nn.ModuleList([TransformerBlock(config) for _ in range(config.n_layers)])
+        
+        self.ff_out = FeedForward(config)
+        
+        self.ln_f = nn.LayerNorm(config.d_half)
+
+        self.lm_head = nn.Linear(config.d_half, config.vocab_size, bias=False)
+        self.lm_head.weight = self.embedding.weight
+        
+        self.apply(self._init_weights)
+        
+    def _init_weights(self, module):
+        if isinstance(module, nn.LayerNorm):
+            nn.init.ones_(module.weight)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Linear):
+            nn.init.xavier_normal_(module.weight)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        
+    def forward(self, x, targets=None, ignore_index=-1):
+        
+        B, S = x.shape
+
+        f = self.embedding(x)
+        ex = f
+        
+        for block in self.transformer_blocks:
+            ex, f = block(ex, f)
+        
+        out = self.ff_out(f)
+        out = self.ln_f(out)
+        
+        logits = self.lm_head(out)
+        
+        if targets is None:
+            return logits, None
+    
+        loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.contiguous().view(-1), ignore_index=ignore_index)
+        return logits, loss
