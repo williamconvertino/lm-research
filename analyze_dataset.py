059b0dab2404cfd8673d150108c3e930691e6e3f
William Convertino <williamconvertino@gmail.com>
2025-03-13 23:59:09 -0400

    Fixed dataset

--- analyze_dataset.py
+++ analyze_dataset.py
@@ -1,5 +1,24 @@
 from dataset.bookcorpus import BookCorpusDataset
 from dataset.tokenizer import Tokenizer
 
+MAX_SEQ_LEN = 128
+
+def get_sample(dataset):
+    for batch in dataset:
+        return batch[0, :]
+
 tokenizer = Tokenizer()
-splits = BookCorpusDataset.get_splits(tokenizer, 128)
+train_dataset = BookCorpusDataset("train", tokenizer, MAX_SEQ_LEN, do_shuffle=False, allow_overlap=True)
+
+val_dataset = BookCorpusDataset("val", tokenizer, MAX_SEQ_LEN, do_shuffle=False, allow_overlap=False)
+
+test_dataset = BookCorpusDataset("test", tokenizer, MAX_SEQ_LEN, do_shuffle=False, allow_overlap=False)
+
+print("Length: ", len(train_dataset))
+print("Sample: ", tokenizer.decode(get_sample(train_dataset).tolist()))
+print("Length: ", len(val_dataset))
+print("Sample: ", tokenizer.decode(get_sample(val_dataset).tolist()))
+print("Length: ", len(test_dataset))
+print("Sample: ", tokenizer.decode(get_sample(test_dataset).tolist()))
+
+# splits = BookCorpusDataset.get_splits(tokenizer, 128)
